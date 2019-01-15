907cbb1a427317276f62b3ab16d1b80f63bc2e98
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2019-01-14 22:23:56 -0500

    Add uninstall command to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,60 +10,27 @@ usage() {
     cat << EOF
 USAGE:
     bootstrap.sh                : install kubekeeper
+    bootstrap.sh uninstall      : uninstall kubekeeper
+
     bootstrap.sh -d,--debug     : run in debug mode
     bootstrap.sh -f,--force     : erase all existing config files
     bootstrap.sh -h,--help      : show this message
     bootstrap.sh -l,--local     : install files from ./
-    bootstrap.sh -s,--silent    : don't say anything, just do it
+    bootstrap.sh -q,--quiet     : don't say anything, just do it
 EOF
 }
 
-main() {
-    # Set config
-    FORCE=false
-    LOCAL=false
-    SILENT=false
-
-    while (( $# ))
-    do
-        case $1 in
-        -d|--debug)
-            set -x
-            shift
-            ;;
-        -f|--force)
-            FORCE=true
-            shift
-            ;;
-        -h|--help)
-            usage
-            exit
-            ;;
-        -l|--local)
-            LOCAL=true
-            shift
-            ;;
-        -s|--silent)
-            SILENT=true
-            shift
-            ;;
-        *)
-            echo "Unknown option $1"
-            usage
-            exit 1
-        esac
-    done
-
-    # Get source
+run_install() {
+        # Get source
     if [[ "$LOCAL" == true ]]; then
         BASEDIR="."
     else
         BASEDIR=$(mktemp -d -t kubekeeper.XXXXXXXXXX)
-        git clone $DEFAULT_REPO $BASEDIR
+        git clone -q $DEFAULT_REPO $BASEDIR
     fi
 
     # Install binaries
-    if [[ "$SILENT" == true ]]; then
+    if [[ "$QUIET" == true ]]; then
         BINDIR=$DEFAULT_BINDIR
     else
         read -p "Binary location? [$DEFAULT_BINDIR] " INPUT_BINDIR
@@ -75,7 +42,7 @@ main() {
     install -m 0755 $BASEDIR/src/kubekeeper $BINDIR/kubekeeper
 
     # Install config files
-    if [[ "$SILENT" == true ]]; then
+    if [[ "$QUIET" == true ]]; then
         CONFIGDIR=$DEFAULT_CONFIGDIR
     else
         read -p "Config location? [$DEFAULT_CONFIGDIR] " CONFIGDIR
@@ -85,27 +52,27 @@ main() {
     mkdir -p $CONFIGDIR
 
     if [[ -f $CONFIGDIR/exclude && "$FORCE" == true ]]; then
-        [[ "$SILENT" == true ]] || echo "Overriding existing exclude config file."
+        [[ "$QUIET" == true ]] || echo "Overriding existing exclude config file."
         mv $CONFIGDIR/exclude $CONFIGDIR/exclude.bck
     fi
     if [[ -f $CONFIGDIR/exclude ]]; then
-        [[ "$SILENT" == true ]] || echo "Exclude config file already exists, skipping."
+        [[ "$QUIET" == true ]] || echo "Exclude config file already exists, skipping."
     else
         cp $BASEDIR/config/exclude $CONFIGDIR/exclude
     fi
 
     if [[ -f $CONFIGDIR/include && "$FORCE" == true ]]; then
-        [[ "$SILENT" == true ]] || echo "Overriding existing include config file."
+        [[ "$QUIET" == true ]] || echo "Overriding existing include config file."
         mv $CONFIGDIR/include $CONFIGDIR/include.bck
     fi
     if [[ -f $CONFIGDIR/include ]]; then
-        [[ "$SILENT" == true ]] || echo "Include config file already exists, skipping."
+        [[ "$QUIET" == true ]] || echo "Include config file already exists, skipping."
     else
         cp $BASEDIR/config/include $CONFIGDIR/include
     fi
 
     # Export config
-    if [[ "$SILENT" != true ]]; then
+    if [[ "$QUIET" != true ]]; then
         echo
         echo "Thank you for installing kubekeeper."
         echo "Please add the following to your .zshrc:"
@@ -124,6 +91,76 @@ main() {
         echo
         echo "Enjoy Kubernetes, we're keeping you safe!"
     fi
+}
+
+run_uninstall() {
+    # Configure verbose mode
+    if [[ "$QUIET" == true ]]; then
+        FLAG=""
+    else
+        FLAG="-v"
+    fi
+
+    # Delete binary
+    rm $FLAG $DEFAULT_BINDIR/kubekeeper
+
+    # Save config
+    if [[ "$FORCE" == true ]]; then
+        rm $FLAG $DEFAULT_CONFIGDIR/exclude
+        rm $FLAG $DEFAULT_CONFIGDIR/include
+    else
+        mv $FLAG $DEFAULT_CONFIGDIR/exclude $DEFAULT_CONFIGDIR/exclude.bck
+        mv $FLAG $DEFAULT_CONFIGDIR/include $DEFAULT_CONFIGDIR/include.bck
+    fi
+}
+
+main() {
+    # Set config
+    FORCE=false
+    LOCAL=false
+    QUIET=false
+
+    while (( $# ))
+    do
+        case $1 in
+        -d|--debug)
+            set -x
+            shift
+            ;;
+        -f|--force)
+            FORCE=true
+            shift
+            ;;
+        -h|--help|help)
+            usage
+            exit
+            ;;
+        -l|--local)
+            LOCAL=true
+            shift
+            ;;
+        -q|--quiet)
+            QUIET=true
+            shift
+            ;;
+        install)
+            run_install
+            exit
+            ;;
+        uninstall)
+            run_uninstall
+            exit
+            ;;
+        *)
+            echo "Unknown option $1"
+            usage
+            exit 1
+        esac
+    done
+
+    # By default, install kubekeeper
+    run_install
+
 }
 
 main $@
